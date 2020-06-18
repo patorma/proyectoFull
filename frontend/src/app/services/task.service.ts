1b2603911972490f05177fda36761dd9af82294d
patorma <pocontreras@ing.ucsc.cl>
2020-06-18 14:34:47 -0400

    prueba de fullStack

---
+++ frontend/src/app/services/task.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root",
+})
+export class TaskService {
+  private URL = "http://localhost:3303/api";
+  constructor(private http: HttpClient) {}
+
+  getTasks() {
+    return this.http.get<any>(this.URL + "/tasks");
+  }
+
+  getPrivateTasks() {
+    return this.http.get<any>(this.URL + "/private-tasks");
+  }
+}
